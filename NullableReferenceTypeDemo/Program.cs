04f5f16582697f913dbe6fc013ca0ba1a43d8eb9
Afroz Chowki <Afroz_Chowki@epam.com>
2021-01-11 11:58:41 +0530

    - added nullable reference type demo project

---
+++ NullableReferenceTypeDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace NullableIntroduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // <SnippetAddQuestions>
+            var surveyRun = new SurveyRun();
+            surveyRun.AddQuestion(QuestionType.YesNo, "Has your code ever thrown a NullReferenceException?");
+            surveyRun.AddQuestion(new SurveyQuestion(QuestionType.Number, "How many times (to the nearest 100) has that happened?"));
+            surveyRun.AddQuestion(QuestionType.Text, "What is your favorite color?");
+
+            //surveyRun.AddQuestion(QuestionType.Text, default);
+            // </SnippetAddQuestions>
+
+            // <SnippetRunSurvey>
+            surveyRun.PerformSurvey(10);
+            // </SnippetRunSurvey>
+
+            // <SnippetWriteAnswers>
+            foreach (var participant in surveyRun.AllParticipants)
+            {
+                Console.WriteLine($"Participant: {participant.Id}:");
+                if (participant.AnsweredSurvey)
+                {
+                    for (int i = 0; i < surveyRun.Questions.Count; i++)
+                    {
+                        var answer = participant.Answer(i);
+                        Console.WriteLine($"\t{surveyRun.GetQuestion(i).QuestionText} : {answer}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\tNo responses");
+                }
+            }
+            // </SnippetWriteAnswers>
+        }
+    }
+}
