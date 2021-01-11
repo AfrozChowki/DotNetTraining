c0d2b30a030094924ac6720e5a343f092994692a
VenkatSai Jeevanagari <Venkatsai_Jeevanagari@epam.com>
2021-01-11 12:23:06 +0530

    Imlemented Switch Expressions

---
+++ DotNetTraining/SwitchExpressionsDemo/Bank.cs
@@ -0,0 +1,20 @@
+﻿
+namespace DotNetTraining.SwitchExpressionsDemo
+{
+    /// <summary>
+    /// Bank class helps to know status of a bank 
+    /// </summary>
+
+    public class Bank
+    {
+        public BankBranchStatus Status { get; set; }
+
+    }
+    //enum gives us status of bank
+    public enum BankBranchStatus
+    {
+        Open,
+        Closed,
+        VIPCustomersOnly
+    }
+}
