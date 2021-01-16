13200bb4e6f5006fca00bdf96f8ac1de9b2ffd48
VenkatSai Jeevanagari <Venkatsai_Jeevanagari@epam.com>
2021-01-16 21:42:04 +0530

    Added Code to update a bank object

--- SwitchExpressionsDemo/BankHelper.cs
+++ SwitchExpressionsDemo/BankHelper.cs
@@ -23,11 +23,11 @@ namespace SwitchExpressionsDemo
         /// <summary>
         /// Helps to generate IFSC Code for a bank
         /// </summary>
-        /// <param name="bank">Takes argument of type Bank Class</param>
+        /// <param name="bankBranch">Takes argument of type Bank Class</param>
         /// <returns>IFSC Code based on bank type</returns>
         public static string GenerateIFSCCode(Type bankBranch)
         {
-            string IfscCode = bankBranch switch
+            var IfscCode = bankBranch switch
             {
                 Type.Public => "AXISP01",
                 Type.Private => "AXISP02",
@@ -39,42 +39,53 @@ namespace SwitchExpressionsDemo
         /// <summary>
         /// Helps to get Interest given by a bank
         /// </summary>
-        /// <param name="bank">Takes argument of type Bank Class</param>
+        /// <param name="ifscCode">Takes argument of IFSC Code of a bank</param>
+        /// <param name="customerCount">Takes argument of customer count of a bank</param>
+        /// <param name="bankType">Takes an argument of type of bank</param>
         /// <returns>Interest rate based on location(IFSC Code) and Type of Bank</returns>
 
         public static double GetInterestRate(string ifscCode, int customerCount, Type bankType)
         {
             var interestRate = (ifscCode, customerCount, bankType) switch
             {
-                ("AXISP01", 1000, Type.Public) => 6.0,
-                ("AXISC03", 900, Type.Commercial) => 2.0,
-                ("AXISP02", 1100, Type.Private) => 8.0,
+                (_, _, Type.Public) => 6.0,
+                (_, 900, Type.Commercial) => 2.0,
+                (_, 1100, Type.Private) => 8.0,
+                ("AXISC02",1100,Type.Commercial) =>2.4,
                 _ => 4.0
 
             };
             return interestRate;
         }
 
-        public static double UsingSwitchGetInterestRate(int customerCount, Type bankType)
+        public static double UsingSwitchGetInterestRate(Bank bank)
         {
-            switch(bankType)
+            switch(bank.BankType,bank.CustomerCount)
             {
-                case Type.Commercial when customerCount <= 1000:
+                case (Type.Commercial,1100):
                 {
                     return 2.2;
                 }
-                case Type.Commercial:
+                case (Type.Commercial,_):
                 {
                     return 2.0;
                 }
-                case Type.Private:
+                case (Type.Private,_):
                     return 8.0;
-                case Type.Public:
+                case (Type.Public,_):
                     return 6.0;
                 default:
                     return 4.0;
 
             }
+        }
+
+        public static void UpdateBank(Bank bank)
+        {
+            bank.CustomerCount = 1100;
+            bank.IFSCCode = "AXISC02";
+            bank.InterestRate = BankHelper.GetInterestRate(bank.IFSCCode, bank.CustomerCount, bank.BankType);
+            bank.Name = "AXIS Special Bank";
         }
     }
 
--- SwitchExpressionsDemo/Program.cs
+++ SwitchExpressionsDemo/Program.cs
@@ -15,6 +15,16 @@ namespace SwitchExpressionsDemo
             // Creating a bank of Commercial Type
             var bank2 = new Bank(Type.Commercial);
             Console.WriteLine(bank2.ToString());
+
+            //using normal switch statement to change interest rate
+            /*bank2.CustomerCount = 1100;
+            bank2.InterestRate = BankHelper.UsingSwitchGetInterestRate(bank2.CustomerCount, bank2.BankType);
+            Console.WriteLine("\nDetails Of Bank After Changed Interest Rate");
+            */
+
+            Console.WriteLine("\nAfter updating Bank2 Details \n");
+            BankHelper.UpdateBank(bank2);
+            Console.WriteLine(bank2.ToString());
             Console.WriteLine();
 
             // Creating a bank of Public Type
