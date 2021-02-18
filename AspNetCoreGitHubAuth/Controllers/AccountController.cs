417135d95dab3eaeb3cb5ec6fa81be917c7139c5
Afroz Chowki <afrozchowki124@gmail.com>
2021-02-18 11:10:45 +0530

    - created OAuth demo application

---
+++ AspNetCoreGitHubAuth/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AspNetCoreGitHubAuth.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login(string returnUrl = "/")
+        {
+            return Challenge(new AuthenticationProperties() { RedirectUri = returnUrl });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            return Redirect(Url.Content("~/"));
+        }
+    }
+}
